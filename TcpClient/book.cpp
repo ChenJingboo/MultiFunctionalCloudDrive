17d6d1f3a61d14673bd595abc626a1918381af4a
chenjingbo <callmecjb@gmail.com>
2024-05-18 10:11:52 +0800

    new layout: filesystem layout

--- TcpClient/book.cpp
+++ TcpClient/book.cpp
@@ -2,5 +2,38 @@
 
 Book::Book(QWidget *parent) : QWidget(parent)
 {
+    m_pFileListW = new QListWidget; // 文件列表，显示所有文件
+    m_pReturnPrePB = new QPushButton("返回");
+    m_pCreateDirPB = new QPushButton("创建文件夹");
+    m_pDelFileOrDirPB = new QPushButton("删除");
+    m_pFlushDirPB = new QPushButton("刷新文件夹");
+    m_pRenameFilePB = new QPushButton("重命名");
 
+    QHBoxLayout *pDirOpVBL = new QHBoxLayout;
+    pDirOpVBL -> addWidget(m_pReturnPrePB);
+    pDirOpVBL -> addWidget(m_pCreateDirPB);
+    pDirOpVBL -> addWidget(m_pFlushDirPB);
+    pDirOpVBL -> addWidget(m_pDelFileOrDirPB);
+    pDirOpVBL -> addWidget(m_pRenameFilePB);
+
+    m_pUploadFilePB = new QPushButton("上传文件");
+    m_pDownloadFilePB = new QPushButton("下载文件");
+    m_pShareFilePB = new QPushButton("分享文件");
+    m_pMoveFilePB = new QPushButton("移动文件");
+    m_pMoveDesDirDB = new QPushButton("目标目录");
+    m_pMoveDesDirDB->setEnabled(false); // 设置目标文件不可点击
+
+    QHBoxLayout *pFileOpVBL = new QHBoxLayout;
+    pFileOpVBL -> addWidget(m_pUploadFilePB);
+    pFileOpVBL -> addWidget(m_pDownloadFilePB);
+    pFileOpVBL -> addWidget(m_pShareFilePB);
+    pFileOpVBL -> addWidget(m_pMoveFilePB);
+    pFileOpVBL -> addWidget(m_pMoveDesDirDB);
+
+    QVBoxLayout *pMainVBL = new QVBoxLayout;
+    pMainVBL -> addLayout(pDirOpVBL);
+    pMainVBL -> addLayout(pFileOpVBL);
+    pMainVBL -> addWidget(m_pFileListW);
+
+    setLayout(pMainVBL);
 }
--- TcpClient/book.h
+++ TcpClient/book.h
@@ -2,6 +2,10 @@
 #define BOOK_H
 
 #include <QWidget>
+#include <QListWidget> // 显示文件的列表
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class Book : public QWidget
 {
@@ -11,6 +15,21 @@ public:
 
 signals:
 
+private:
+    QListWidget *m_pFileListW;      // 文件列表，显示所有文件
+
+    QPushButton *m_pReturnPrePB;    // 返回上一路径
+    QPushButton *m_pCreateDirPB;    // 创建文件夹
+    QPushButton *m_pFlushDirPB;     // 刷新文件夹
+    QPushButton *m_pDelFileOrDirPB; // 删除文件或文件夹
+
+    QPushButton *m_pRenameFilePB;   // 重命名文件
+    QPushButton *m_pUploadFilePB;   // 上传文件
+    QPushButton *m_pDownloadFilePB; // 下载文件
+    QPushButton *m_pShareFilePB;    // 分享文件
+
+    QPushButton *m_pMoveFilePB;     // 移动文件
+    QPushButton *m_pMoveDesDirDB;   // 移动目标目录
 };
 
 #endif // BOOK_H
